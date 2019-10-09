675eae5e3c1a90ac624bd595d5cfc1712b1b3950
Ahmad Bani jamali <ahmad.banijamali@gmail.com>
2019-10-09 12:04:06 +0300

    Update AWSClusterDeployment.sh

--- AWSClusterDeployment.sh
+++ AWSClusterDeployment.sh
@@ -19,12 +19,12 @@ echo
 echo “#####Installing kubectl#####”
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 echo
 
 echo
 echo “#####Installing Docker#####”
-apt-get install docker.io -y
+sudo apt-get install docker.io -y
 echo
 
 
@@ -47,7 +47,7 @@ echo
 echo “#####Installing kops on ubuntu instance#####”
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
-mv kops-linux-amd64 /usr/local/bin/kops
+sudo mv kops-linux-amd64 /usr/local/bin/kops
 echo
 
 echo
