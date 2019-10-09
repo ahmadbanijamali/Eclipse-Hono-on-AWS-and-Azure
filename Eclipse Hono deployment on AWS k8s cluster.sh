41331ac45a36f928d989aaecfc85555cd9c3d47e
Ahmad Banijamali <ahmad.banijamali@gmail.com>
2019-10-09 15:37:22 +0300

    First commit Message

---
+++ Eclipse Hono deployment on AWS k8s cluster.sh
@@ -0,0 +1,28 @@
+echo “####Eclipse Hono on AWS k8s####”
+echo ”####installing Helm####”
+curl -LO https://git.io/get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+helm init
+#kubectl create serviceaccount --namespace kube-system tiller
+#kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+# kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+echo Done!
+
+echo
+echo “####Clone the Hono Helm chart####”
+wget http://download.eclipse.org/hono/eclipse-hono-1.0-M7-chart.tar.gz
+tar -zxvf eclipse-hono-1.0-M7-chart.tar.gz -C /root/
+cd
+echo
+
+echo 
+echo “####Deploying Hono####”
+cd eclipse-hono-1.0-M7/deploy/ 
+helm dep update helm/eclipse-hono 
+helm template --name hono --namespace hono --output-dir . helm/eclipse-hono 
+kubectl create namespace hono  
+kubectl config set-context $(kubectl config current-context) --namespace=hono  
+find . -path "./eclipse-hono/*" -name crd*.yaml -exec kubectl apply -f {} \;  
+kubectl apply -f ./eclipse-hono -R
+echo “####success!!###””
