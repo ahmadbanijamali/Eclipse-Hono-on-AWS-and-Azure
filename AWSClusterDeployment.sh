2e314f339c2c42d5092e5dbca959423a9e616641
Ahmad Bani jamali <ahmad.banijamali@gmail.com>
2019-10-09 12:02:23 +0300

    Update AWSClusterDeployment.sh

--- AWSClusterDeployment.sh
+++ AWSClusterDeployment.sh
@@ -11,8 +11,8 @@ echo
 echo “#####Installing AWSCLI#####”
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
 sudo apt install unzip python
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 echo
 
 echo
