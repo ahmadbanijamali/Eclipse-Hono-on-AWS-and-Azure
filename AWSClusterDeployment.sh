3037611747f2fdbeedf58a7358c3798b964a4cd0
Ahmad Bani jamali <ahmad.banijamali@gmail.com>
2019-10-09 12:49:32 +0300

    Update AWSClusterDeployment.sh

--- AWSClusterDeployment.sh
+++ AWSClusterDeployment.sh
@@ -4,11 +4,11 @@
 set -e
 
 echo
-echo #####Creating Ubuntu EC2 instance#####
+echo “#####Creating Ubuntu EC2 instance#####"
 apt-get update
 
 echo
-echo #####Installing AWSCLI#####
+echo "#####Installing AWSCLI#####"
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
 apt install unzip python
 unzip awscli-bundle.zip
@@ -16,20 +16,20 @@ unzip awscli-bundle.zip
 echo
 
 echo
-echo #####Installing kubectl#####
+echo "#####Installing kubectl#####"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 echo
 
 echo
-echo #####Installing Docker#####
+echo "#####Installing Docker#####"
 apt-get install docker.io -y
 echo
 
 
 #echo
-#echo #####Installing Minikube#####
+#echo "#####Installing Minikube#####"
 #curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
 #chmod +x minikube 
 #mv minikube /usr/local/bin/
@@ -38,30 +38,31 @@ echo
 #echo
 
 echo
-echo #####Configuring the role#####
+echo "#####Configuring the role#####"
 aws configure
 echo
 
 echo
-echo #####Installing kops on ubuntu instance#####
+echo "#####Installing kops on ubuntu instance#####"
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 echo
 
 echo
-echo #####We need to have a Route53 private hosted zone#####
+echo "#####We need to have a Route53 private hosted zone#####"
 echo 
 
 echo
-echo #####Creating an S3 bucket
+echo "#####Creating an S3 bucket"
 aws s3 mb s3://dev.k8s.appstacleoulu.fi
 sleep 10
 echo Done!
 
 echo
-echo #####Expose environment variable#####
-export KOPS_STATE_STORE=s3://dev.k8s.appstacleoulu.fi
+echo "#####Expose environment variable#####"
+KOPS_STATE_STORE="s3://dev.k8s.appstacleoulu.fi"
+export KOPS_STATE_STORE
 sleep 5
 echo Done!
 
