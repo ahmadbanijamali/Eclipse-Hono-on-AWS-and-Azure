6b7d952ae012eace62c493563f92d4e1948ad57c
Ahmad Bani jamali <ahmad.banijamali@gmail.com>
2019-11-01 10:59:46 +0200

    Update Azure_k8s.sh

--- Azure_k8s.sh
+++ Azure_k8s.sh
@@ -21,7 +21,7 @@ echo
 echo "#####Installing kubectl#####"
 cd ~/ 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
+#curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 echo
