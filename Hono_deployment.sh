16a1af9bc5810dceea9e3606c54ffd3137fce165
Ahmad Bani jamali <ahmad.banijamali@gmail.com>
2019-11-01 15:49:37 +0200

    Update Hono_deployment.sh

--- Hono_deployment.sh
+++ Hono_deployment.sh
@@ -32,7 +32,8 @@ echo
 
 echo 
 echo “####Deploying Eclipse Hono 1.0.0####”
-cd /root/eclipse-hono-1.0.0 
+alias Hono_Dir="cd /root/eclipse-hono-1.0.0"
+Hono_Dir
 mkdir resources
 helm dep update eclipse-hono/
 helm template --name hono --namespace hono --output-dir resources eclipse-hono/
